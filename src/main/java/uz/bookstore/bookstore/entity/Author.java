76c41237c912609cd309a973fc08aad5673f0db1
eeslambec <eeslambec@gmail.com>
2024-03-08 20:04:59 +0500

    removed wild cards and unused imports.

--- src/main/java/uz/bookstore/bookstore/entity/Author.java
+++ src/main/java/uz/bookstore/bookstore/entity/Author.java
@@ -1,7 +1,17 @@
 package uz.bookstore.bookstore.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 

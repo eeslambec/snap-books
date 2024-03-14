a37c406d547ec6ab8bd34cc12053f3518505ba9b
eeslambec <141905418+eeslambec@users.noreply.github.com>
2024-03-15 04:17:53 +0500

    Delete src/main/java/uz/bookstore/bookstore/exception/NotFoundException.java

--- src/main/java/uz/bookstore/bookstore/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package uz.bookstore.bookstore.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-
-}

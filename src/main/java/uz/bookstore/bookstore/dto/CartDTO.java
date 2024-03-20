383ac2328e01fdc3026493eed5fc2c8908aea842
sayfiddin <abdivahobovsayfiddin@gmail.com>
2024-03-20 15:11:58 +0500

    CartDTO fix

--- src/main/java/uz/bookstore/bookstore/dto/CartDTO.java
+++ src/main/java/uz/bookstore/bookstore/dto/CartDTO.java
@@ -1,16 +1,11 @@
 package uz.bookstore.bookstore.dto;
 
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import uz.bookstore.bookstore.entity.Book;
-
 import java.util.List;
 
-@Setter
+@Getter
 @AllArgsConstructor
 public class CartDTO {
     private Long id;
